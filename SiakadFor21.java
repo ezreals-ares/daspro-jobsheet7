4841d659e4b912d0a9cd42bd6ee104c0ed8ad834
ezreals-ares <nawaf303@gmail.com>
2024-10-10 20:36:29 +0700

    Memodifikasi kode program

--- SiakadFor21.java
+++ SiakadFor21.java
@@ -1,22 +1,35 @@
 import java.util.Scanner;
+
 public class SiakadFor21 {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int jumlahLulus = 0, jumlahTidakLulus = 0;
+        
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan Nilai Mahasiswa ke- " + i + ": ");
             nilai = sc.nextDouble();
+            
             if (nilai > tertinggi) {
                 tertinggi = nilai;
             }
-            if (nilai < terendah){
+            if (nilai < terendah) {
                 terendah = nilai;
             }
-
+            
+            if (nilai >= 60) {
+                jumlahLulus++;
+            } else {
+                jumlahTidakLulus++;
+            }
         }
+        
         System.out.println("Nilai Tertinggi : " + tertinggi);
         System.out.println("Nilai Terendah : " + terendah);
+        System.out.println("Jumlah Mahasiswa Lulus : " + jumlahLulus);
+        System.out.println("Jumlah Mahasiswa Tidak Lulus : " + jumlahTidakLulus);
+        
         sc.close();
     }
 }
